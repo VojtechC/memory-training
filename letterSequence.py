b9227dc00ae714c12e739d8e2ccf3f62e81f36b4
VojtechC <capik.vojta@seznam.cz>
2024-02-05 18:12:15 +0100

    changed

--- letterSequence.py
+++ letterSequence.py
@@ -2,31 +2,31 @@
 
 import random
 
-SEQUENCE_LENGTH = 8
-lettersList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Z']
-
-finalSequence = ""
-for _ in range(SEQUENCE_LENGTH):
-  finalSequence += random.choice(lettersList)
+def generateSequence(length):
+    lettersList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return ''.join(random.choice(lettersList) for _ in range(length))
 
 def displaySequence(sequence):
-  for i in range(SEQUENCE_LENGTH):
-    print(sequence[i],end="")
-    if i % 2 == 1:
-      print(" ",end="")
-  
-  
-def checkSequence(sequence):
-  userSequence = input().upper()
-  if userSequence == sequence:
-    print(f'You\'re right! | Sequence: {sequence}  | Your guess: {userSequence}')
-  else:
-    print(f'Wrong! | Sequence: {sequence} | Your guess: {userSequence}')
-   
-  
+    print("Generated sequence: ",end="")
+    for i in range(len(sequence)):
+        print(sequence[i], end="")
+        if i % 2 == 1:
+            print(" ", end="")
+
+def checkSequence(userInput, sequence):
+    if userInput.upper() == sequence:
+        print(f'You\'re right! | Sequence: {sequence}  | Your guess: {userInput}')
+    else:
+        print(f'Wrong! | Sequence: {sequence} | Your guess: {userInput}')
+
+#Main program
+SEQUENCE_LENGHT = 8
+finalSequence = generateSequence(SEQUENCE_LENGHT)
+
 displaySequence(finalSequence)
 
-input("Press enter when you are ready")
-print(100*'\n')
+input("\nPress enter when you are ready")
+print(100 * '\n')
 
-checkSequence(finalSequence)
+userSequence = input("Enter your guess: ")
+checkSequence(userSequence, finalSequence)
